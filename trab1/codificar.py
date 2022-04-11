34bab2fd4aa64c1e57cf67af56fad8b3d616f52c
Randerson Lemos <randerson.lemos@gmail.com>
2022-04-11 16:40:36 -0300

    Atualizado codificar.py para aceitar multiplos planos

--- trab1/codificar.py
+++ trab1/codificar.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import copy
 import argparse
@@ -19,36 +20,35 @@ def int_to_byte(num):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Script para inserção de mensagem escondida em images (Esteganografia)')
-    parser.add_argument('-imagem_entrada', required=True, help="Imagem utilizada para inserção de mensagem escondida")
+    parser.add_argument('-imagem_entrada', required=True, help="Imagem utilizada para inserção de mensagem a ser escondida")
     parser.add_argument('-texto_entrada',  required=True, help="Mensagem a ser escondida na imagem")
-    parser.add_argument('-plano_bits',     required=True, type=int, help="Plano de bits: ou 0 ou 1 ou 2")
+    parser.add_argument('-planos_bits',    required=True, help="Planos de bits: ou 0 ou 1 ou 2, ou combinações desses valores separados por ':'")
 
     args = parser.parse_args()
-    imagem = args.imagem_entrada
-    mat = cv2.imread(imagem)
+    imagem_entrada = args.imagem_entrada
+    imagem = cv2.imread(imagem_entrada)
 
-    original = mat
-    modified = copy.copy(mat)
-    shape = original.shape
+    planos_bits = sorted(list(map(int, args.planos_bits.split(':')))) # Começar sempre do plano de bits menos significativo
 
     with open(args.texto_entrada, 'r') as fh:
         texto = fh.read()
 
-    plano_bits = args.plano_bits
-
-    bytes_lst = word_to_byte(texto)
-    bites = ''.join(bytes_lst)
+    bytes = word_to_byte(texto) # Lista de bytes
+    bites = ''.join(bytes) # Sequência de bits
     
+    shape = imagem.shape
     nrows, ncols, nchannels = shape
+    nplanos = len(planos_bits)
 
-    if len(bytes_lst)*8 > nrows*ncols*nchannels:
+    if len(bytes)*8 > nrows*ncols*nchannels*nplanos:
         raise(Exception('Text is to long...'))
     
-    positions = itertools.product(range(nrows), range(ncols), range(nchannels))
+    cartesian = itertools.product(range(nrows), range(ncols), range(nchannels), range(nplanos))
 
-    for bit, position in zip(bites, positions):
-        pixel = original[position]
-        byte = int_to_byte(pixel)
+    for bit, (row, col, channel, plano_bits) in zip(bites, cartesian):
+        position = (row, col, channel)
+        pixel = imagem[position]
+        byte = int_to_byte(pixel) # Pega valor do pixel em representação de bits
         #print(byte, end=' ')    
         if plano_bits == 0:
             byte = byte[:-1] + bit
@@ -57,12 +57,17 @@ if __name__ == '__main__':
         elif plano_bits == 2:
             byte = byte[:-3] + bit + byte[-2:]
         else:
-            raise(Exception('Only 0, 1, and 2 are allowed for "plano_bits"'))
+            raise(Exception('Apenas 0, 1, e 2 são permitidos para "planos_bits"'))
 
         #print(bit, '\n'+byte, end='\n---\n')    
 
-        modified[position] = int(byte, 2)
+        imagem[position] = int(byte, 2)
 
-    name = imagem.split('/')[-1].split('.')[0]
-    name = 'out/{}m_plano{}.png'.format(name, plano_bits)
-    cv2.imwrite(name, modified)
+    try:
+        os.mkdir('out')
+    except FileExistsError:
+        pass
+
+    name = imagem_entrada.split('/')[-1].split('.')[0]
+    name = 'out/{}m_plano{}.png'.format(name, ''.join(map(str, planos_bits)))
+    cv2.imwrite(name, imagem)

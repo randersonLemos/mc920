2b679aafa8d13930abb94ff8c0feb3bd0a80a08f
Randerson A. Lemos <randerson.lemos@gmail.com>
2022-05-31 11:09:46 -0300

    Starting BlocksDifferences class

--- trab3/main.py
+++ trab3/main.py
@@ -64,8 +64,7 @@ class PixelsDifferences(ABCPixels):
         left  = self.brightness(frame) 
         right = self.brightness(self.last)
 
-        diff = left - right
-
+        diff = abs(left - right)
         viol = diff > self.max_pixel_distance
 
         nume = viol.sum()
@@ -90,20 +89,64 @@ class PixelsDifferences(ABCPixels):
         return 'Diferença entre pixels'
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Script para busca de mudanças abruptas em videos ')
-    parser.add_argument('-video_entrada', required=True, help='Video que se deseja buscas mudancas abruptas')
+class BlocksDifferences(ABCPixels):
+    def __init__(self, max_block_mse, max_block_num_per, block_dim):
+        super().__init__()
 
-    args  = parser.parse_args()
-    path = args.video_entrada
+        self.max_block_mse = max_block_mse
+        self.max_block_num_per = max_block_num_per
 
-    name = path.split('/')[-1]
-    stem = name[:-4]
-    suffix = name[-3:]
+        if block_dim not in ['8x8', '16x16']:
+            raise ValueError('Parameter block_dim must be 8x8 or 16x16')
+
+        self.block_dim = block_dim
+
+        if block_dim == '8x8':
+            self.block_inc = 8
+        else:
+            self.block_inc = 16
 
 
+    def analyze_frame(self, frame):
+        self.num_analyzed_frames += 1
+        if self.last is None:
+            self.last = frame
 
-    mpds = [50, 150, 250] # max_pixel_distances
+            self.height = frame.shape[0]
+            self.width  = frame.shape[1]
+
+            self.block_num_height = int(self.height / self.block_inc)
+            self.block_num_width  = int(self.width  / self.block_inc)
+            self.block_num = self.block_num_height*self.block_num_width
+            return
+
+        blocks = self._get_blocks(frame)
+        print('I am in analyze frame')
+        import IPython; IPython.embed()
+        self.is_different(frame)
+
+    
+    def _get_blocks(self, frame):
+        blocks = []
+        inc = self.block_inc
+        for h in range(self.block_num_height):
+            for w in range(self.block_num_width):
+                block = frame[h*inc : (h+1)*inc:, w*inc : (w+1)*inc]
+                blocks.append(block)
+        return blocks 
+
+
+    def is_different(self, frame):
+        left  = self.brightness(frame) 
+        right = self.brightness(self.last)
+
+
+    def strategy_name(self):
+        return 'Diferença entre blocos'
+
+
+def main_pixels_differences(path, stem):
+    mpds = [50, 100, 150, 200, 250] # max_pixel_distances
     mpps = [0.10, 0.20, 0.30, 0.40, 0.50, 0.60] # max_pixel_num_per
 
 
@@ -134,7 +177,7 @@ if __name__ == '__main__':
         frame_width = pd.width
         if pd.violation:
             fps = 10
-            writer = cv2.VideoWriter('out/{}.{}'.format(pd.suggested_stem(stem), suffix),cv2.VideoWriter_fourcc('M','J','P','G'), 30, (frame_width,frame_heigh))
+            writer = cv2.VideoWriter('out/{}.{}'.format(pd.suggested_stem(stem), 'mp4'),cv2.VideoWriter_fourcc('M','J','P','G'), 30, (frame_width,frame_heigh))
             keys = sorted( list( pd.violation.keys() ) )
             for key in keys:
                 tup = pd.violation[key]
@@ -182,3 +225,44 @@ if __name__ == '__main__':
         plt.tight_layout(pad=1.10)
 
         plt.savefig('out/{}.{}'.format(pd.suggested_stem(stem), 'png'))
+        
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Script para busca de mudanças abruptas em videos ')
+    parser.add_argument('-video_entrada', required=True, help='Video que se deseja buscas mudancas abruptas')
+
+    args  = parser.parse_args()
+    path = args.video_entrada
+    name = path.split('/')[-1]
+    stem = name[:-4]
+
+    #main_pixels_differences(path, stem)
+
+
+    mbms = [50, 100, 150, 200, 250] # max_block_mse
+    mbps = [0.10, 0.20, 0.30, 0.40, 0.50, 0.60] # max_block_num_per
+
+
+    product = itertools.product(mbms, mbps)
+    for mbm, mbp in product:
+        cap = cv2.VideoCapture(path)
+        bd = BlocksDifferences(max_block_mse=mbm, max_block_num_per=mbp, block_dim='8x8')
+        
+        while True:
+            ret, frame = cap.read() # Captura frame por frame
+
+            if ret == True:   
+                #hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV) # Converte de BGR para HSV
+                #value = hsv[:,:,2]
+                #cv2.imshow('frame', value); cv2.waitKey(5) 
+                bd.analyze_frame(frame)
+
+            else:
+                break
+
+
+    #    print('---')
+    #    print(pd.num_analyzed_frames)
+    #    print(len(pd.violation))
+
+

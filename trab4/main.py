c87b9d56550fd7b51114880a1df47569ae7dcf97
Randerson Lemos <randerson.lemos@gmail.com>
2022-06-29 08:17:12 -0300

    Lagrange interpolation finish

--- trab4/main.py
+++ trab4/main.py
@@ -17,6 +17,7 @@ class Interpolation:
                 return mat[y, x, ch]
         return 0
 
+
     @classmethod
     def bilinear(cls, w, h, ch, mat, width, height):
         x, y = int(w), int(h)
@@ -38,6 +39,7 @@ class Interpolation:
     @classmethod
     def P(cls, t):
         return t if t > 0 else 0
+
 
     @classmethod
     def R(cls, s):
@@ -64,12 +66,54 @@ class Interpolation:
                     return 0
         return px
 
-class Projection:
-    def apply(self, T, M):
-        raise NotImplemented
+
+    @classmethod
+    def L(cls, n, x, y, ch, dx, dy, mat, width, height):
+        x_1 = x - 1
+        x = x
+        x1  = x + 1
+        x2 = x + 2
+
+        yn_2 = y + n - 2
+
+        if ( x_1 >= 0 ) and ( x_1 < width ) \
+            and ( x >= 0 ) and ( x < width ) \
+            and ( x1 >= 0 ) and ( x1 < width ) \
+            and ( x2 >= 0 ) and ( x2 < width ) \
+            and ( yn_2 >= 0 ) and ( yn_2 < height):
+                a = -dx*( dx-1 )*( dx-2 )*mat[ yn_2, x_1, ch ]
+                b = ( dx+1 )*( dx-1 )*( dx-2 )*mat[ yn_2, x, ch ]
+                c = -dx*( dx+1 )*( dx-2 )*mat[ yn_2, x1, ch ]
+                d = dx*( dx+1 )*( dx-1 )*mat[ yn_2, x2, ch ]
+                return ( a/6 )+( b/2 )+( c/2 )+( d/6 ) + 1e-9 # Valor adicionado apenas para usar os L's no if
+                                                              # da função lagrange
+        else:
+            return 0
 
 
-class Geometric(Projection):
+    @classmethod
+    def lagrange(cls, w, h, ch, mat, width, height):
+        x, y = int(w), int(h)
+        dx = w - x
+        dy = h - y
+
+        L1 = cls.L(1, x, y, ch, dx, dy, mat, width, height)
+        L2 = cls.L(2, x, y, ch, dx, dy, mat, width, height)
+        L3 = cls.L(3, x, y, ch, dx, dy, mat, width, height)
+        L4 = cls.L(4, x, y, ch, dx, dy, mat, width, height)
+
+        if ( L1 == 0) or ( L2 == 0 ) or ( L3 == 0) or ( L4 == 0 ):
+            return 0
+
+        a = -dy*( dy-1 )*( dy-2 )*L1
+        b = ( dy+1 )*( dy-1 )*( dy-2 )*L2
+        c = -dy*( dy+1 )*( dy-2 )*L3
+        d = dy*( dy+1 )*( dy-1 )*L4
+
+        return ( a/6 )+( b/2 )+( c/2 )+( d/6 )
+        
+
+class Geometric:
     @classmethod
     def set_inter_func(cls, inter_func):
         cls.inter_func = inter_func
@@ -166,6 +210,7 @@ inter_func = {}
 inter_func['nearneighbours'] = Interpolation.nearneighbours
 inter_func['bilinear'] = Interpolation.bilinear
 inter_func['bicubic'] = Interpolation.bicubic
+inter_func['lagrange'] = Interpolation.lagrange
 inter_keys = inter_func.keys()
 
 
@@ -175,7 +220,7 @@ if __name__ == '__main__':
     parser.add_argument('-e'             , required=False, type=float,  help='Fator de escala a ser aplicado na imagem')
     parser.add_argument('-d'             , required=False,              help='largura e altura')
     parser.add_argument('-a'             , required=False, type=float,  help='Angulo de rotacao em graus a ser aplicado na imagem')
-    parser.add_argument('-m'             , required=False, help='Método de interpolacao. Opcoes: {}'.format(inter_keys))
+    parser.add_argument('-m'             , required=True, help='Método de interpolacao. Opcoes: {}'.format(inter_keys))
     
     args = parser.parse_args()
     M = cv2.imread(args.imagem_entrada)
